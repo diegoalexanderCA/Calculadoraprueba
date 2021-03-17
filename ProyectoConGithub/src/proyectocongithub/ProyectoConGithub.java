a1bb5f580a71ab0fc8630f8ceab7961b4cf21573
Oboe <Oboe@DESKTOP-MCI6F98>
2021-03-17 20:25:57 +0100

    1DO COMMIT borrado un paquete, una clase y añado un mensaje en clase ProyectoconGithub.

--- ProyectoConGithub/src/proyectocongithub/ProyectoConGithub.java
+++ ProyectoConGithub/src/proyectocongithub/ProyectoConGithub.java
@@ -16,6 +16,8 @@ public class ProyectoConGithub {
      */
     public static void main(String[] args) {
         System.out.println("Primer mensaje del proyecto");
+        System.out.println("He borrado un paquete, una clase y añado este mensaje, veamos con git los captura");
+                
                 
     }
     
